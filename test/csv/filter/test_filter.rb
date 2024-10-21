34e20efdf50370d8a0cf6fb909623a1f5492fab4
BurdetteLamar <burdettelamar@yahoo.com>
2024-10-21 07:45:26 -0500

    More

--- test/csv/filter/test_filter.rb
+++ test/csv/filter/test_filter.rb
@@ -4,14 +4,17 @@
 require 'minitest/autorun'
 require 'csv'
 require 'tempfile'
+require 'shellwords'
 
 class TestFilter < Minitest::Test
 
+  # Names and aliases for options.
   CliOptionNames = {
     # Input options.
     converters: %w[--converters],
     field_size_limit: %w[--field_size_limit],
     headers: %w[--headers],
+    header_converters: %w[--header_converters],
     input_row_sep: %w[--input_row_sep --in_row_sep],
     input_col_sep: %w[--input_col_sep --in_col_sep],
     unconverted_fields: %w[--unconverted_fields],
@@ -73,6 +76,13 @@ class TestFilter < Minitest::Test
     filepath
   end
 
+  def execute_in_cli(filepath, cli_options_s = '')
+    command = "cat #{filepath} | ruby bin/filter #{cli_options_s}"
+    capture_subprocess_io do
+      system(command)
+    end
+  end
+
   def get_act_values(filepath, cli_option_name, primary_option, options)
     cli_options = [{name: cli_option_name, value: primary_option.cli_argument_value}]
     options.each do |option|
@@ -82,12 +92,9 @@ class TestFilter < Minitest::Test
     cli_options.each do |cli_option|
       cli_options_s += " #{cli_option[:name]}"
       value = cli_option[:value]
-      cli_options_s += " #{value}" unless value.nil?
+      cli_options_s += " #{Shellwords.escape(value)}" unless value.nil?
     end
-    command = "cat #{filepath} | ruby bin/filter #{cli_options_s}"
-    act_out_s, act_err_s = capture_subprocess_io do
-      system(command)
-    end
+    act_out_s, act_err_s = execute_in_cli(filepath, cli_options_s)
     [act_out_s, act_err_s]
   end
 
@@ -96,52 +103,51 @@ class TestFilter < Minitest::Test
     options.each do |option|
       api_options[option.sym] = option.api_argument_value
     end
-    csv_s = File.read(filepath)
-    filtered_s = ''
-    CSV.filter(csv_s, filtered_s, **api_options) do |row|
-    end
-    return filtered_s
+    act_in_s = File.read(filepath)
+    exp_out_s = get_via_api(act_in_s, **api_options)
+    return exp_out_s
   end
 
   def verify_via_cli(test_method, option_name, exp_out_pat: '', exp_err_pat: '')
     Dir.mktmpdir do |dirpath|
       sym = option_name.to_sym
       filepath = csv_filepath('', dirpath, sym)
-      command = "cat #{filepath} | ruby bin/filter #{option_name}"
-      act_out_s, act_err_s = capture_subprocess_io do
-        system(command)
-      end
+      act_out_s, act_err_s = execute_in_cli(filepath, option_name)
       assert_match(exp_out_pat, act_out_s, test_method)
       assert_match(exp_err_pat, act_err_s, test_method)
     end
   end
 
-  def verify_via_api(test_method, csv_s, options = [])
+  def assert_all(test_method, exp_out_s, act_out_s, act_err_s)
+    assert_empty(act_err_s, test_method)
+    assert_equal(exp_out_s.strip, act_out_s.strip, test_method)
+  end
+
+  def get_via_api(act_in_s, **api_options)
+    exp_out_s = ''
+    CSV.filter(act_in_s, exp_out_s, **api_options) {|row| }
+    exp_out_s
+  end
+
+  def verify_via_api(test_method, act_in_s, options = [])
     Dir.mktmpdir do |dirpath|
       if options.empty?
-        # Make expected output string (via API).
-        exp_out_s = ''
-        CSV.filter(csv_s, exp_out_s) do |row|
-        end
-        # Make actual output and error strings (via CLI).
-        filepath = csv_filepath(csv_s, dirpath, :no_options)
-        command = "cat #{filepath} | ruby bin/filter"
-        act_out_s, act_err_s = capture_subprocess_io do
-          system(command)
-        end
+        # Get expected output string (via API).
+        exp_out_s = get_via_api(act_in_s)
+        # Get actual output and error strings (via CLI).
+        filepath = csv_filepath(act_in_s, dirpath, :no_options)
+        act_out_s, act_err_s = execute_in_cli(filepath)
         # Verify.
-        assert_empty(act_err_s, test_method)
-        assert_equal(exp_out_s, act_out_s, test_method)
+        assert_all(test_method, exp_out_s, act_out_s, act_err_s)
       else
         primary_option = options.shift
-        filepath = csv_filepath(csv_s, dirpath, primary_option.sym)
+        filepath = csv_filepath(act_in_s, dirpath, primary_option.sym)
         primary_option.cli_option_names.each do |cli_option_name|
           # Get expected output string (from API).
           exp_out_s = get_exp_value(filepath, primary_option, options)
           # Get actual output and error strings (from CLI).
           act_out_s, act_err_s = get_act_values(filepath, cli_option_name, primary_option, options)
-          assert_empty(act_err_s, test_method)
-          assert_equal(exp_out_s, act_out_s, test_method)
+          assert_all(test_method, exp_out_s, act_out_s, act_err_s)
         end
       end
     end
@@ -201,6 +207,20 @@ class TestFilter < Minitest::Test
     csv_s = make_csv_s
     options = [
       Option.new(:headers, headers)
+    ]
+    verify_via_api(__method__, csv_s, options)
+  end
+
+  def test_option_header_converters
+    header_converters = %i[downcase symbol]
+    rows = [
+      ['Foo', 'Bar'],
+      ['0', 1],
+    ]
+    csv_s = make_csv_s(rows: rows)
+    options = [
+      Option.new(:headers, nil),
+      Option.new(:header_converters, header_converters)
     ]
     verify_via_api(__method__, csv_s, options)
   end
@@ -275,10 +295,9 @@ class TestFilter < Minitest::Test
   def test_option_q
     quote_char = "Z"
     rows = [
-      %w[foo 0],
-      %w['bar' 1],
-      %w["baz", 2],
-      %w[ZbatZ, 2],
+      ['foo', 0],
+      ["ZbarZ", 1],
+      ['"baz"', 2],
     ]
     csv_s = make_csv_s(rows: rows)
     options = [
@@ -289,16 +308,14 @@ class TestFilter < Minitest::Test
 
   # Make sure we can pass multiple options.
   def test_multiple_options
-    row_sep = 'X'
-    col_sep = 'Y'
-    quote_char = 'Z'
-    csv_s = make_csv_s(row_sep: row_sep, col_sep: col_sep)
+    row_sep = 'R'
+    col_sep = 'C'
+    act_in_s = make_csv_s(row_sep: row_sep, col_sep: col_sep)
     options = [
       Option.new(:row_sep, row_sep),
       Option.new(:col_sep, col_sep),
-      Option.new(:quote_char, quote_char),
     ]
-    verify_via_api(__method__, csv_s, options)
+    verify_via_api(__method__, act_in_s, options)
   end
 
 end
